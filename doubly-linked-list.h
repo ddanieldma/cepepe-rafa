67654e29199de513cc79516b40068693e530fbd1
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2024-04-29 09:08:47 -0300

    atualizado arquivo de DLL

--- doubly-linked-list.h
+++ doubly-linked-list.h
@@ -5,14 +5,8 @@ using std::cout;
 using std::endl;
 using std::string;
 
-// LISTA, pilha e fila
-// Torre de Hanoi com pilha
-// Lista circular: o ultimo elemento aponta para o primeiro
-// Lista duplamente encadeada: todo nó aponta para o anterior e para o próximo (buble sort)
 
-// LISTA DUPLAMENTE ENCADEADA:
-
-
+// Estrutura de nó da lista duplamente encadeada.
 typedef struct Node{
     int iPayload;
     Node *ptrNext;
@@ -20,31 +14,27 @@ typedef struct Node{
 } Node;
 
 
+void iguais();
 Node *createNode(int);
 void insertFront(Node **, int);
 void insertEnd(Node **, int);
 void displayList(Node *);
 void insertAfter(Node*, int);
 void deleteNode(Node**, Node*);
+void insertBefore(Node**, Node*, int);
+Node *searchNodeByValue(Node**, int);
+void DeleteNodeByValue(Node**, int);
 
 
-// Driver code.
-int main(){
-    Node *head = nullptr;
-    displayList(head);
-    cout << "==============" << endl;
-    insertEnd(&head, 0);
-    insertEnd(&head, 1);
-    insertEnd(&head, 3);
-    insertEnd(&head, 7);
-    insertEnd(&head, 10);
-    insertEnd(&head, 13);
-    displayList(head);
-    cout << "==============" << endl;
-    return 0;
+void iguais(){
+    cout << "===============================" << endl;
+    
+    return;
 }
 
 Node *createNode(int iPayload){
+    // Cria e inicializa um nó.
+    
     Node *temp = (Node *)malloc(sizeof(Node));
     temp->iPayload = iPayload;
     temp->ptrNext = nullptr;
@@ -54,6 +44,7 @@ Node *createNode(int iPayload){
 }
 
 void displayList(Node *node){
+    // Exibe todos os elementos da lista.
 
     // Se o nó passado é apenas um ponteiro nulo.
     if (node == nullptr){
@@ -79,25 +70,25 @@ void displayList(Node *node){
     cout << endl;
 }
 
-void insertFront(Node **head, int iPayload){
+void insertFront(Node** head, int iPayload){
     // Insere novo valor no começo da lista.
     
     Node *newNode = createNode(iPayload);
 
     // Se a lista já tiver algum elemento.
-    if (*head != nullptr){
+    if ((*head) != nullptr){
         newNode->ptrNext = (*head);
         newNode->ptrNext->ptrPrev = newNode;
-        *head = newNode;
+        (*head) = newNode;
     }
     // Se não, newNode passa a ser nosso único elemento.
     else
-        *head = newNode;
+        (*head) = newNode;
 
     return;
 }
 
-void insertEnd(Node **head, int iPayload){
+void insertEnd(Node** head, int iPayload){
     Node *newNode = createNode(iPayload);
 
     if (*head == nullptr){
@@ -107,15 +98,16 @@ void insertEnd(Node **head, int iPayload){
 
     Node *temp = (*head);
 
-    // Percorremos a lista até seu fim(ptrNext do ultimo nó é NULL)
     while (temp->ptrNext != nullptr)
         temp = temp->ptrNext;
 
-    newNode->ptrPrev = temp; // newNode aponta para o fim da lista
-    temp->ptrNext = newNode; // Antigo último elemento aponta para o novo nó
+    newNode->ptrPrev = temp; 
+    temp->ptrNext = newNode;
 }
 
 void insertAfter(Node* ptrLocation, int iPayload){
+    // Insere um nó com o valor recebido após um nó determinado.
+    
     if (ptrLocation == nullptr){
         cout << "Location é NULL" << endl;
         return;
@@ -144,6 +136,8 @@ void deleteNode(Node** head, Node*ptrDelete){
         return;
     }
 
+    cout << "Deletando valor: " << ptrDelete->iPayload << endl;
+
     // Se formos deletar o primeiro elemento.
     if (ptrDelete == (*head)){
         (*head)->ptrNext->ptrPrev = nullptr;
@@ -169,6 +163,74 @@ void deleteNode(Node** head, Node*ptrDelete){
     return;
 }
 
-// Exercício 1 elaborar a função válida void insertBefore(Node*, int).
-// Exercício 2 elaborar a função válida Node* searchNodeByValue(Node*, int).
-// Exercício 3 elaborar a função válida void deleteNodebyValue(Node**, int).
+void insertBefore(Node** head, Node* ptrLocation, int iPayload){
+    // Insere um nó com o valor recebido antes de um nó determinado.
+    
+    if(ptrLocation == nullptr){
+        cout << "ptrLocation é NULL" << endl;
+
+        return;
+    }
+    if((*head) == nullptr){
+        cout << "Lista vazia" << endl;
+
+        return;
+    }
+    
+    Node* newNode = createNode(iPayload);
+
+    // Se o ptrPrev do ptrLocation for nullptr estamos na head da lista.
+    if(ptrLocation->ptrPrev == nullptr){
+        (*head)->ptrPrev = newNode;
+        newNode->ptrNext = (*head)->ptrNext;
+        (*head) = newNode;
+
+        return;
+    }
+
+    // Corrigir nó a ser inserido.
+    newNode->ptrNext = ptrLocation;
+    newNode->ptrPrev = ptrLocation->ptrPrev;
+
+    // Corrigir ponto de inserção.
+    ptrLocation->ptrPrev = newNode;
+
+    // Corrigr nó antes.
+    newNode->ptrPrev->ptrNext = newNode;
+
+    return;
+}
+
+Node *searchNodeByValue(Node **head, int iValue){
+    // Procura um nó na lista pelo valor nele contido e o retorna. 
+    
+    if((*head) == nullptr){
+        cout << "Lista vazia." << endl;
+        return nullptr;
+    }
+
+    Node* temp = (*head);
+
+    while(temp != nullptr){
+        if(temp->iPayload == iValue)
+            return temp;
+
+        temp = temp->ptrNext;
+    }
+
+    cout << "Valor não encontrado." << endl;
+    return nullptr;
+}
+
+void DeleteNodeByValue(Node** head, int iValue){
+    // Deleta um nó da lista pelo valor nele contido.
+
+    // Não precisamos nos preocupar com as verificações e casos especiais
+    // por que as funções de pesquisa pelo valor e de deletar um nó já 
+    // o fazem.
+
+    Node* search = searchNodeByValue(head, iValue);
+    deleteNode(head, search);
+
+    return;
+}
