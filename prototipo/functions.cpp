43ca775d03f4f0c53a23664c2d65f12dc3a53c19
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2024-05-13 09:06:52 -0300

    pasta renomeada

--- prototipo/functions.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using std::cout;
-using std::endl;
-
-// Função declarada no arquivo functions.h.
-void soma(int iValor1, int iValor2){
-    cout << "Soma dos valores: " << iValor1 + iValor2 << endl;
-}
--- prototipo/functions.h
+++
@@ -1,4 +0,0 @@
-// Dizendo ao compilador para adicionar essa função uma única vez.
-#pragma once
-
-void soma(int, int);
