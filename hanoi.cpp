9519f0f57773f402b8cd94bb8710a43822dfea96
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2024-06-12 09:00:34 -0300

    ultima aula ;-;

--- hanoi.cpp
+++ hanoi.cpp
@@ -11,15 +11,26 @@ void moveDisk(char, char, unsigned int);
 void solveHanoi(unsigned int, char, char, char);
 
 int main(){
-    unsigned int uiNumber = 1;
+    unsigned int uiNumberOfDisks = 1;
 
     cout << "Digite o numero de discos: ";
-    cin >> uiNumber;
-    solveHanoi(uiNumber, 'A', 'B', 'C');
+    cin >> uiNumberOfDisks;
+    solveHanoi(uiNumberOfDisks, 'A', 'C', 'B');
 
     return 0;
 }
 
-void moveDisk(char fromRod, char toRod, unsigned int Disk){
-    
+void moveDisk(char fromRod, char toRod, unsigned int uiDisk){
+    cout << "Move o disco " << uiDisk << " de " << fromRod << " para " << toRod << endl;
+}
+
+void solveHanoi(unsigned int uiNumberOfDisks, char fromRod, char toRod, char auxRod){
+    if(uiNumberOfDisks == 1){
+        moveDisk(fromRod, toRod, 1);
+        return;
+    }
+
+    solveHanoi(uiNumberOfDisks - 1, fromRod, auxRod, toRod);
+    moveDisk(fromRod, toRod, uiNumberOfDisks);
+    solveHanoi(uiNumberOfDisks - 1, auxRod, toRod, fromRod);
 }
